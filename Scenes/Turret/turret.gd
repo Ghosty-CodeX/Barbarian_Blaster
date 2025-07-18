b38e46b76f0b651183218f70f8e0aca5f97e9c2b
Ghosty-CodeX <brandonbosch8@gmail.com>
2025-07-18 10:00:22 +0400

    Projectiles updated

--- Scenes/Turret/turret.gd
+++ Scenes/Turret/turret.gd
@@ -1,7 +1,10 @@
 extends Node3D
 
 @export var projectile : PackedScene
+@onready var turret_barrel: MeshInstance3D = $TurretBase/TurretBarrel
 
-func _ready() -> void:
+
+func _on_timer_timeout() -> void:
 	var shot = projectile.instantiate()
 	add_child(shot)
+	shot.global_position = turret_barrel.global_position
