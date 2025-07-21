b69c53cec749e0e6e6fde1c4829c4a1c133de8da
Ghosty-CodeX <brandonbosch8@gmail.com>
2025-07-21 12:44:10 +0400

    Player reward system

--- Scenes/UserInterface/victory_layer.gd
+++ Scenes/UserInterface/victory_layer.gd
@@ -1,5 +1,13 @@
 extends CanvasLayer
 
+@onready var star_1: TextureRect = %Star1
+@onready var star_2: TextureRect = %Star2
+@onready var star_3: TextureRect = %Star3
+@onready var health_label: Label = %HealthLabel
+@onready var money_label: Label = %MoneyLabel
+
+@onready var base = get_tree().get_first_node_in_group("base")
+@onready var bank = get_tree().get_first_node_in_group("bank")
 
 func _on_restart_button_pressed() -> void:
 	get_tree().reload_current_scene()
@@ -7,3 +15,12 @@ func _on_restart_button_pressed() -> void:
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
+
+func victory() -> void:
+	visible = true
+	if base.max_health == base.current_health:
+		star_2.modulate = Color.WHITE
+		health_label.visible = true
+	if bank.starting_gold >= 500:
+		star_3.modulate = Color.WHITE
+		money_label.visible = true
